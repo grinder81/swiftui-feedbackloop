c5382276aa27ca03a91fb5800daa995b018fffbb
Md Al Mamun <md.almamun@loblaw.ca>
2019-11-17 19:18:59 -0500

    Initial Commit

---
+++ swiftui-feedbackloopTests/swiftui_feedbackloopTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_feedbackloopTests.swift
+//  swiftui-feedbackloopTests
+//
+//  Created by MD AL MAMUN (LCL) on 2019-11-17.
+//  Copyright © 2019 MD AL MAMUN. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_feedbackloop
+
+class swiftui_feedbackloopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
